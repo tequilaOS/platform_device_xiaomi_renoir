6b9599041fac24dea3992d9d0ab119f99b73462b
Arian <arian.kulmer@web.de>
2021-11-07 19:17:23 +0100

    renoir: Set bootloader board name
    Change-Id: Ied8de2941ccc455c7755f50e3843eadde59f1ad8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/renoir
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := renoir
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/renoir_QGKI.config
 
