cab1ce9eafcb0a25225e4340e8772f97f2061817
Erfan Abdi <erfangplus@gmail.com>
2021-12-18 07:37:10 +0330

    renoir: Add hotword priv app permissions
    Change-Id: Ic936e852cb77fe47c0ce69465b7a3717c2619b7e

--- device.mk
+++ device.mk
@@ -374,6 +374,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
