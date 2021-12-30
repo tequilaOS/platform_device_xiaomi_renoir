e2fa100123702978514242ae6b38aed74df83ae6
ArixElo <patroxgamer@gmail.com>
2021-12-30 21:26:21 +0100

    sm8350-common: Drop Lineage packages

--- common.mk
+++ common.mk
@@ -270,10 +270,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
