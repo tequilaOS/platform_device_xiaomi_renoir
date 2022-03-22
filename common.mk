b7381d74f442cb7ffc3227d495060dca00a63f7a
ArixElo <patroxgamer@gmail.com>
2022-03-22 10:07:02 +0100

    sm8350-common: Add missing power@1.2 vendor HIDL
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- common.mk
+++ common.mk
@@ -384,6 +384,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
