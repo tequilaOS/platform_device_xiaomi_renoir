9e8fe2ed8a01ea9081a58617815a946b9dd31124
IacobIonut01 <paulionut2003@gmail.com>
2022-04-05 16:57:50 +0000

    sm8350-common: Include display mapper@1.1

--- common.mk
+++ common.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 PRODUCT_PACKAGES += \
     vendor.display.config@1.15.vendor \
@@ -174,6 +175,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti.vendor \
     libqdMetaData \
     libtinyxml \
+    gralloc.default \
     memtrack.default
 
 PRODUCT_COPY_FILES += \
