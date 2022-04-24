069f5de1c5e97c463405e6041b8cd346783b3f08
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-04-24 15:51:18 +0200

    renoir: Import public libraries
    * From renoir-user-11-RKQ1.201112.002-V12.5.7.0.RKIMIXM-release-keys
    
    SystemConfig: Failed to read public libraries file /vendor/etc/public.libraries.txt
    SystemConfig: java.io.FileNotFoundException: /vendor/etc/public.libraries.txt: open failed: ENOENT (No such file or directory)

--- device.mk
+++ device.mk
@@ -397,6 +397,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
