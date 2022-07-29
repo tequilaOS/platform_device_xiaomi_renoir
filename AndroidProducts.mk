9155fbca14a10ccc6454b2e6ca25e4603453f2fc
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-07-29 07:10:11 +0000

    renoir: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/streak_renoir.mk
+    $(LOCAL_DIR)/tequila_renoir.mk
 
 COMMON_LUNCH_CHOICES := \
-    streak_renoir-user \
-    streak_renoir-userdebug \
-    streak_renoir-eng
+    tequila_renoir-user \
+    tequila_renoir-userdebug \
+    tequila_renoir-eng
--- tequila_renoir.mk
+++ tequila_renoir.mk
@@ -12,16 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
 # Inherit some common ProjectStreak stuff.
-$(call inherit-product, vendor/streak/config/common_full_phone.mk)
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := streak_renoir
-
-# ProjectStreak properties
-STREAK_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := tequila_renoir
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
