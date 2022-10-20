a7f8d9f2c348a4cefbce9f42c8c2d016e04eb8ff
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-10-20 09:56:54 +0200

    renoir: Time for tequila!
    Change-Id: I38b5b328a0d0f147203d71b0d70070825467a9d3

--- tequila_renoir.mk
+++ tequila_renoir.mk
@@ -11,13 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common tequila stuff.
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9G
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := tequila_renoir
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
