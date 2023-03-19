f15cceeea84555bf60522c7365a3266cf20939c1
Arian <arian.kulmer@web.de>
2023-03-19 17:24:27 +0100

    renoir: Update from V14.0.3.0.TKIMIXM
    Change-Id: Ieb311162c2c382507647d08428c29a95f87356ce

--- tequila_renoir.mk
+++ tequila_renoir.mk
@@ -24,10 +24,10 @@ PRODUCT_SYSTEM_NAME := renoir_global
 PRODUCT_SYSTEM_DEVICE := renoir
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="renoir_global-user 12 SKQ1.211006.001 V13.0.9.0.SKIMIXM release-keys" \
+    PRIVATE_BUILD_DESC="renoir_global-user 13 TKQ1.220829.002 V14.0.3.0.TKIMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Xiaomi/renoir_global/renoir:12/SKQ1.211006.001/V13.0.9.0.SKIMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/renoir_global/renoir:13/TKQ1.220829.002/V14.0.3.0.TKIMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
