5626d28f6b61c03f5c6547ca521bf6b96126a3c4
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-06-19 16:54:42 +0200

    renoir: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_renoir.mk
+    $(LOCAL_DIR)/tequila_renoir.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_renoir-user \
-    lineage_renoir-userdebug \
-    lineage_renoir-eng
+    tequila_renoir-user \
+    tequila_renoir-userdebug \
+    tequila_renoir-eng
